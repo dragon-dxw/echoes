445fee69065d0a7266c4ee91cf3882d7e7fb1227
Bluebottle <isobel.hooper@gmail.com>
2019-09-23 22:17:15 +0100

    WIP: Trying to create a Vision admin page using django_reverse_admin
    Not complete yet; I think I'll have writeup sections be a separate thing,
    but I'm not sure how best to present them.

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django_reverse_admin import ReverseModelAdmin
 
 # Register your models here.
+from .models import Person, SummitDate, WriteupSection, Vision
+
+class VisionAdmin(ReverseModelAdmin):
+    inline_type = 'tabular'
+    inline_reverse = ['visionary',
+                      'guide',
+                      'date']
+
+admin.site.register(Vision, VisionAdmin)
