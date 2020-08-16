cdcd706fd3a02c354c46ec0c6ad404095bc1a798
Bluebottle <isobel.hooper@gmail.com>
2020-08-16 15:27:37 +0100

    Register Vision and Volume with admin
    Also give them useful string representations for the admin lists.

--- core/models.py
+++ core/models.py
@@ -7,6 +7,15 @@ from django.db.models import TextField, CharField, ForeignKey
 class Volume(models.Model):
     date = CharField(max_length=100)
 
+    def __str__(self):
+        return self.date
+
 class Vision(models.Model):
     account = TextField()
     volume = ForeignKey(Volume, on_delete=models.deletion.CASCADE)
+
+    def __str__(self):
+        """
+        For now, use the first line of the vision text.
+        """
+        return str(self.account).split("\n")[0]
