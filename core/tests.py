5768b922f175de4a8f5262763a37bac647d73c8c
Dragon (David McKee) <dragon@dxw.com>
2021-09-12 17:17:49 +0100

    Block publication if no permission. Absence of tests.

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+from django.contrib.auth.models import Group
+
+class MyTest(TestCase):
+    fixtures = ["dumpdata.json"]
+
+    def test_output_okay(self):
+	    pass
+
+#   def test_should_create_group(self):
+#       group = Group.objects.get(pk=1)
+#       self.assertEqual(group.name, "appusers")
